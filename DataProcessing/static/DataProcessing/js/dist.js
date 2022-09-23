c3ff3a1cd4599d02f00d70d01f41d313f618f88d
Jiantao Zeng <rdstihz@qq.com>
2022-09-23 16:41:40 +0800

    åsimple pendlum

--- DataProcessing/static/DataProcessing/js/dist.js
+++ DataProcessing/static/DataProcessing/js/dist.js
@@ -806,9 +806,93 @@ export class SimplePendlum {
     }
 
     start() {
-        $(`#final-btn3`).click(e => {
+        $(`#simple-pendulum-final-btn3`).click(e => {
             window.location.replace("/");
         });
+        $(`#simple-pendulum-final-btn2`).click(e => {
+            this.work();
+        });
+    }
+
+    work() {
+        let data = this.get_input();
+        //console.log(data);
+
+        let n = 5;
+        let x = [], y = [];
+        for (let i = 0; i < n; i++) {
+            x.push(data[i][0]);
+            y.push((data[i][1] / 50) * (data[i][1] / 50)); //t^2
+        }
+
+        let sx = 0, sy = 0;
+        let sxx = 0, syy = 0, sxy = 0;
+        for (let i = 0; i < n; i++) {
+            sx += x[i], sy += y[i];
+            sxx += x[i] * x[i], syy += y[i] * y[i];
+            sxy += x[i] * y[i];
+        }
+
+        //斜率
+        let k = (sxy - sx * sy / n) / (sxx - sx * sx / n);
+        //截距
+        let b = sy / n - k * sx / n;
+        //相关系数
+        let r = (n * sxy - sx * sy) / (Math.sqrt(n * sxx - sx * sx) * Math.sqrt(n * syy - sy * sy));
+
+        console.log(k, b, r);
+        let g = 4 * Math.PI * Math.PI / k;
+        console.log(g);
+
+        let g0 = 979.4;
+
+        $(`#simple-pendulum-date-result`).html(g.toFixed(1));
+        $(`#simple-pendulum-date-error`).html(((g - g0) / g0 * 100).toFixed(1));
+        $(`#simple-pendulum-jieju`).html((-b / k).toFixed(1));
+        $(`#simple-pendulum-xgxs`).html(r.toFixed(5));
+
+
+        //绘制图像
+        this.$img = document.getElementById("simple-pendulum-figure");
+        console.log(this.$img);
+        $.ajax({
+            url: "http://127.0.0.1:8000/exp/getfigure3/",
+            type: "GET",
+            data: {
+                'k': k,
+                'b': b,
+                'x': this.array_to_str(x, n),
+                'y': this.array_to_str(y, n),
+                'n': n,
+                'state': '99787fdsf',
+            },
+            success: resp => {
+                if (resp.result === "success") {
+                    console.log("success");
+                    this.$img.src = resp.src;
+                }
+            }
+        });
+
+    }
+
+    get_input() {
+        let data = [];
+        for (let i = 1; i <= 5; i++) {
+            let l = +$(`#simple-pendulum-length${i}`).val();
+            let t = +$(`#simple-oendulum-cycle${i}`).val();
+            data.push([l, t]);
+        }
+        return data;
+    }
+
+    array_to_str(x, n) {
+        let res = '';
+        for (let i = 0; i < n; i++) {
+            if (i) res += ','
+            res += x[i];
+        }
+        return res;
     }
 
 }
--- DataProcessing/views.py
+++ DataProcessing/views.py
@@ -123,5 +123,52 @@ def getfigure2(request):
         'src': src,
     })
 
+
+def getfigure3(request):
+    data = request.GET
+
+    x = map(float, data['x'].split(","))
+    y = map(float, data['y'].split(","))
+    k = float(data['k'])
+    b = float(data['b'])
+    x = list(x)
+    y = list(y)
+    plt.scatter(x, y)
+
+    x = [65] + x + [110]
+    y_fit = [(k * i + b) for i in x]
+
+    plt.plot(x, y_fit)
+
+    plt.xlabel("L(cm)")
+    plt.ylabel("T^2 (s^2)")
+    plt.title("simple pendlum T^2-L figure")
+
+    # 调整坐标轴
+    ax = plt.gca()
+
+    # 去掉右边和上边的线
+    ax.spines['right'].set_color("none")
+    ax.spines['top'].set_color("none")
+
+    # 让x轴从0开始
+    #ax.spines['left'].set_position(('data', 12))
+    # file_name = datetime.now().strftime("%Y%m%d%H%M%S" + data['state'] +".png")
+    # plt.savefig(str(settings.BASE_DIR) + "/static/temp/" + file_name)
+
+    sio = BytesIO()
+    plt.savefig(sio, format='png', bbox_inches='tight', pad_inches=0.0)
+    data = base64.encodebytes(sio.getvalue()).decode()
+    src = 'data:image/png;base64,' + str(data)
+    # # 记得关闭，不然画出来的图是重复的
+
+    plt.close()
+
+    return JsonResponse({
+        'result': "success",
+        'src': src,
+    })
+
+
 def calculator(request):
     return render(request, 'Toolbox/calculator.html')
